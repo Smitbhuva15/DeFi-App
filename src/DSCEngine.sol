088c2fac0f0d5a450a6ad7efde3d39b2637c93da
Smitbhuva15 <itssmit123@gmail.com>
2025-05-09 17:21:29 +0000

    added bacis function info

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -3,6 +3,24 @@
 pragma solidity ^0.8.18;
 
 contract DSCEngine {
+
+    function depositCollateralAndMintDsc() external {}
+
+    function redeemCollateralForDsc() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDsc() external {}
+
+    function  burnDsc() external {}
+
+    function liquidate() external {}
+
+    function getMinHealthFactor() external {}
+
+ 
+
+
     
 }
 
